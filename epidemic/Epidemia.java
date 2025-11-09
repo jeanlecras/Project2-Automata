570b4b49178b15188eec24a0a9ce9371e8635197
ishaac <xf1r3Z@gmail.com>
2025-11-09 16:40:35 +0100

    add TEST and fix simulation(mortality)

--- epidemic/Epidemia.java
+++ epidemic/Epidemia.java
@@ -120,6 +120,16 @@ public class Epidemia
     }
     
     /**
+     * 
+     * public test method of hasIllNeighbor 
+     * is only for testing this method 
+     */
+
+    public boolean testHasIllNeighbor(int y, int x) {
+        return hasIllNeighbor(y, x);
+    }
+
+    /**
      * Determine the next state of an individual at a give position
      */
     private char nextState(int y, int x) {
@@ -193,27 +203,42 @@ public class Epidemia
      * start the disease (and set the mortality) and pass days until the disease is gone
      */
     private void simulation(double mortality) {
-        startDisease(mortality, 5, 5);
+        int center = this.dimension / 2;
+        startDisease(mortality, center, center);
         while (hasOne('X')) {
             nextDay();
         }
     }
-    
+
+    /**
+     * j'ajoute deux methode pour pouvoir tester rapidement les methode depuis le main
+     * 
+     */
+
+    public char[][] getMatrix() {
+        return this.matrix;
+    }
+
+    public void setMatrix(char[][] newMatrix) {
+        this.matrix = newMatrix;
+        this.dimension = newMatrix.length;
+    }
+
+
     /**
      * Tests
      */
     public static void main(String[] args) {
-        //Epidemia epi1 = new Epidemia();
-        //Epidemia epi2 = new Epidemia(50, 0.8, 0.5);
-        //epi1.simulation();
-        //epi2.simulation();
-
-
         // test of method hasOne
-        // System.out.println(epi1.hasOne('I'));
-        // System.out.println(epi2.hasOne('I'));
+        System.out.println("=== TEST hasOne ===");
+
+        Epidemia epi1 = new Epidemia(2, 1.0, 0.0);
+        System.out.println(epi1.hasOne('S'));
+        System.out.println(epi1.hasOne('X'));
+        
 
         // test of method StartDisease
+        System.out.println("=== TEST StartDisease ===");
             // test 1 - on cell 'S'
         Epidemia startTest1 = new Epidemia(5, 1.0, 0.0);
         startTest1.startDisease(0.1, 2, 2);
@@ -238,6 +263,118 @@ public class Epidemia
             // test 3 - startDisease() random
         Epidemia startTest3 = new Epidemia(5, 1.0, 0.0);
         startTest3.startDisease(); // random version
-        System.out.println(startTest3.hasOne('X'));
+        System.out.println(startTest3.hasOne('X')); // true = a letter X is found, false = no one letter 'X' is found
+
+
+        // test of hasIllNeighbor
+        System.out.println("=== TESTS hasIllNeighbor ===");
+
+        Epidemia testHasNeighbor = new Epidemia(3, 1.0, 0.0); // tout 'S'
+            // On va placer des malades ('X') à des positions précises
+        testHasNeighbor.matrix[1][1] = 'X'; // cellule centrale infectée
+
+            // Cas 1 : cellule juste au-dessus du malade
+        boolean result1 = testHasNeighbor.testHasIllNeighbor(0, 1); // au-dessus de (1,1)
+        if (result1 == true) {System.out.println("Test1 (haut du malade) est bon !");} else {System.out.println("Le TEST n'est pas bon");}
+
+            // Cas 2 : cellule à gauche du malade
+        boolean result2 = testHasNeighbor.testHasIllNeighbor(1, 0); // gauche de (1,1)
+        if (result2 == true) {System.out.println("Test2 (gauche du malade) est bon !");} else {System.out.println("Le TEST n'est pas bon");}
+
+            // Cas 3 : cellule diagonale (pas voisine directe)
+        boolean result3 = testHasNeighbor.testHasIllNeighbor(0, 0); // diagonale haut-gauche
+        if (result3 == false) {System.out.println("Test3 (diagonale du malade, doit être faux) est bon !");} else {System.out.println("Le TEST n'est pas bon");}
+        
+        // test for nextState
+        System.out.println("=== TEST nextState ===");
+
+        Epidemia testNextState = new Epidemia(3, 1.0, 0.0);
+        testNextState.setMatrix(new char[][] {
+            {'S', 'X', 'S'},
+            {'S', 'S', 'S'},
+            {'S', 'S', 'S'}
+        });
+
+            // Cas 1 : cellule saine à gauche d’un malade
+        char res1 = testNextState.nextState(0, 0);
+        if (res1 == 'X') {System.out.println("Test 1 : S voisin de X devient X"); } else { System.out.println("Le test n'est pas bon");}
+        
+
+            // Cas 2 : cellule malade devient D ou I
+        testNextState.mortality = 1.0; // mortalité totale -> doit mourir
+        char res2 = testNextState.nextState(0, 1);
+        if (res2 == 'D') {System.out.println("Test 2 : malade devient 'D'"); } else { System.out.println("Le test n'est pas bon");}
+
+        testNextState.mortality = 0.0; // pas de mortalité -> doit guérir
+        char res3 = testNextState.nextState(0, 1);
+        if (res3 == 'I') {System.out.println("Test 3 : malade devient I"); } else { System.out.println("Le test n'est pas bon");}
+        
+
+            // Cas 3 : cellule isolée (aucun malade)
+        char res4 = testNextState.nextState(2, 2);
+        if (res4 == 'S') {System.out.println("Test 4 : S sans voisin reste S"); } else { System.out.println("Le test n'est pas bon");}
+
+        
+        // test of nextDay()
+        System.out.println("\n=== TEST nextDay ===");
+
+        Epidemia epi2 = new Epidemia(3, 1.0, 0.0);
+        epi2.setMatrix(new char[][] {
+            {'S', 'X', 'S'},
+            {'S', 'S', 'S'},
+            {'S', 'S', 'S'}
+        });
+
+        System.out.println("Avant nextDay :");
+        epi2.displayEpidemia();
+
+        epi2.mortality = 0.0;
+        epi2.nextDay();
+
+        System.out.println("Après nextDay :");
+        epi2.displayEpidemia();
+
+        // Vérifie que les voisins directs de 'X' sont maintenant infectés
+        if (epi2.getMatrix()[0][0] == 'X' || epi2.getMatrix()[0][2] == 'X' || epi2.getMatrix()[1][1] == 'X') {
+            System.out.println("Propagation correcte");
+        }
+        else {
+            System.out.println("Aucune propagation détectée");
+        }
+        
+        // test of passDay(n)
+        System.out.println("\n=== TEST passDays ===");
+
+        Epidemia epi3 = new Epidemia(3, 1.0, 0.0);
+        epi3.setMatrix(new char[][] {
+            {'S', 'X', 'S'},
+            {'S', 'S', 'S'},
+            {'S', 'S', 'S'}
+        });
+
+        System.out.println("Avant passDays(2) :");
+        epi3.displayEpidemia();
+        epi3.mortality = 0.0;
+
+        epi3.passDays(2); // doit propager 2 jours de suite
+        System.out.println("Après passDays(2) :");
+        epi3.displayEpidemia();
+
+        // test of simulation
+        System.out.println("\n=== TEST simulation() ===");
+
+        Epidemia epi4 = new Epidemia(5, 1.0, 0.0);
+        epi4.mortality = 0.3;
+        epi4.simulation();
+        System.out.println("Simulation terminée !");
+
+        // test of simulation(double mortality)
+        System.out.println("\n=== TEST simulation(mortality) ===");
+
+        Epidemia epi5 = new Epidemia(5, 1.0, 0.0);
+        epi5.simulation(0.5);
+        System.out.println("Simulation terminée avec mortalité = 0.5");
+
+
     }
 }
